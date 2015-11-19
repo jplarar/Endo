9c951d99cbc51d284fe5f8a00ff6aa359a05c6e0
Juan Paulo Lara Rodríguez <jplarar@gmail.com>
2015-11-18 20:03:37 -0600

    fixes

--- Endo/src/EndoBundle/Entity/MedicalRecord.php
+++ Endo/src/EndoBundle/Entity/MedicalRecord.php
@@ -88,7 +88,7 @@ class MedicalRecord
     protected $emergencyRelation;
 
     /**
-     * @ORM\Column(type="string", nullable=true
+     * @ORM\Column(type="string", nullable=true)
      */
     protected $doctorName;
 
